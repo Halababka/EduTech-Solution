54f28dd73339b056e39326df54f47609b16b169a
einz <iljapopov01@mail.ru>
2024-03-26 15:16:26 +0300

    feat : добавлен запрос на получение курсов пользователя

--- routes/courses.routes.js
+++ routes/courses.routes.js
@@ -7,6 +7,7 @@ import { authenticateToken } from "../middleware/authenticateToken.js";
 
 router.use(authenticateToken);
 router.get("/", coursesController.allCourses);
+router.get('/users/:userId/courses', coursesController.getCoursesByUserId);
 router.post("/", coursesController.newCourse);
 router.get("/:id", coursesController.getCourse);
 router.delete("/:id", coursesController.deleteCourse);

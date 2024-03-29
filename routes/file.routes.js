08efce8805d1f5d0cd3e85ca65758db4454d0cd9
einz <iljapopov01@mail.ru>
2024-03-29 10:16:10 +0300

    feat: добавлен токен

--- routes/file.routes.js
+++ routes/file.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { FileController } from '../controller/file.controller.js';
+import { authenticateToken } from "../middleware/authenticateToken.js";
 
 const filesController = new FileController();
 const router = express.Router();
 
+router.use(authenticateToken)
 router.post('/upload', filesController.uploadFile);
 
 export default router;

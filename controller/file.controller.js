7ea23b59cd1b0928db3a20d01853cfb61fc7ff75
einz <iljapopov01@mail.ru>
2024-03-31 15:24:53 +0300

    feat: добавлена логика записи данных файла в бд

--- controller/file.controller.js
+++ controller/file.controller.js
@@ -1,6 +1,7 @@
-import multer from 'multer';
-import multerS3 from 'multer-s3';
+import multer from "multer";
+import multerS3 from "multer-s3";
 import { s3 } from "../s3.js";
+import { client } from "../db.js";
 
 export class FileController {
     async uploadFile(req, res) {
@@ -9,21 +10,45 @@ export class FileController {
                 s3: s3,
                 bucket: process.env.S3_BUCKET_NAME,
                 contentType: multerS3.AUTO_CONTENT_TYPE,
-                acl: 'public-read',
+                acl: "public-read",
                 key: (req, file, cb) => {
                     cb(null, `uploads/${Date.now()}-${file.originalname}`);
-                },
+                }
             }),
-        }).single('file');
+            limits: { fileSize: 1024*1024*1024} // 1GB лимит файла
+        }).array("files", 50); // Позволяет загружать до 50 файлов, имя поля 'files'
 
-        upload(req, res, (err) => {
+        upload(req, res, async (err) => {
             if (err) {
-                console.error('Error uploading file:', err);
-                return res.status(500).json({ error: 'Error uploading file' });
+                console.error("Error uploading files:", err);
+                return res.status(500).json({err});
             }
 
-            console.log('File uploaded successfully');
-            return res.status(200).json({ message: 'File uploaded successfully' });
+            try {
+                const uploadedFiles = req.files; // Массив загруженных файлов
+                // Сохранение информации о каждом файле в базе данных
+                const savedFiles = await Promise.all(uploadedFiles.map(async (file) => {
+                    // Создание записи в базе данных для файла
+                    const savedFile = await client.materials.create({
+                        data: {
+                            name: file.originalname,
+                            description: "",
+                            mime_type: file.mimetype,
+                            path: file.location, // Путь к файлу в S3
+                            owner: {connect: {id: parseInt(req.user.id)}}, // Идентификатор пользователя, загрузившего файл (если нужно)
+                            size: file.size
+                        }
+                    });
+                    console.log(`File download to BD: ${savedFile.name}`)
+                    return savedFile;
+                }));
+
+                console.log("Files uploaded successfully");
+                return res.status(200).json({message: "Files uploaded successfully", files: savedFiles});
+            } catch (error) {
+                console.error("Error saving files to database:", error);
+                return res.status(500).json({error: "Error saving files to database"});
+            }
         });
     }
 }

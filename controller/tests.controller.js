64eacb57b85ff6f9bd3fd43840c21de725bb4d3e
max <@max>
2024-04-08 16:42:29 +0300

    feat: subjects for tests CRud

--- controller/tests.controller.js
+++ controller/tests.controller.js
@@ -1,9 +1,58 @@
-import {DB} from "../db/db.js";
+import {client} from '../db.js';
+import dbErrorsHandler from "../utils/dbErrorsHandler.js";
 
-const db = new DB()
+const isSubjectExist = async (res, name) => {
+    try {
+        const results = await client.subject.findMany({
+            where: {
+                name: name,
+            },
+        });
+        return Boolean(results.length);
+    } catch (e) {
+        res.status(500).json({error: dbErrorsHandler(e)})
+    }
+}
 
 export class TestsController {
     async getTest(req, res) {
         res.json(1);
+    }
+
+    async createSubject(req, res) {
+        const {name} = req.body;
+
+        if (!name) {
+            res.status(400).json({error: 'Необходимо указать название темы'})
+            return
+        }
+
+        if ((name.split(" ").join("")) === '') {
+            res.status(400).json({error: 'Необходимо указать название темы'})
+            return
+        }
+
+        if (await isSubjectExist(res, name)) {
+            res.status(409).json({error: 'Такая тема уже существует'})
+            return
+        }
+
+        let newSubject;
+        try {
+            newSubject = await client.subject.create({
+                data: {
+                    name: name
+                },
+            });
+        } catch (e) {
+            res.status(500).json({error: dbErrorsHandler(e)})
+            return
+        }
+        res.json(newSubject);
+    }
+
+    async getSubjects(req, res) {
+        const subjects = await client.subject.findMany()
+        res.json(subjects);
     }
 }
--- routes/tests.routes.js
+++ routes/tests.routes.js
@@ -1,29 +1,37 @@
 import express from 'express';
-import { TestsController } from '../controller/tests.controller.js';
+import {TestsController} from '../controller/tests.controller.js';
 
 const testsController = new TestsController();
 const router = express.Router();
-import { authenticateToken } from '../middleware/authenticateToken.js'
+import {authenticateToken} from '../middleware/authenticateToken.js'
 
 router.use(authenticateToken)
-// /tests/
-router.post('/', testsController.getTest)
-router.get('/:id', testsController.getTest)
-router.put('/:id', testsController.getTest)
-router.delete('/:id', testsController.getTest)
-// /tests/themes
-router.post('/themes', testsController.getTest)
-router.get('/themes/:theme_id', testsController.getTest)
-router.put('/themes/:theme_id', testsController.getTest)
-router.delete('/themes/:theme_id', testsController.getTest)
-// /tests/themes/questions
-router.post('/themes/:theme_id/questions', testsController.getTest)
-router.get('/themes/:theme_id/questions/:question_id', testsController.getTest)
-router.put('/themes/:theme_id/questions/:question_id', testsController.getTest)
-router.delete('/themes/:theme_id/questions/:question_id', testsController.getTest)
-// /tests/themes/questions/answers
-router.post('/themes/:theme_id/questions/:question_id', testsController.getTest)
-router.get('/themes/:theme_id/questions/:question_id', testsController.getTest)
-router.put('/themes/:theme_id/questions/:question_id/answers/:answer_id', testsController.getTest)
-router.delete('/themes/:theme_id/questions/:question_id/answers/:answer_id', testsController.getTest)
+
+router.post('/subjects', testsController.createSubject)
+router.get('/subjects', testsController.getSubjects)
+router.get('/subjects/:id', testsController.getTest)
+router.put('/subjects/:id', testsController.getTest)
+router.delete('/subjects/:id', testsController.getTest)
+
+
+// // /tests/
+// router.post('/', testsController.getTest)
+// router.get('/:id', testsController.getTest)
+// router.put('/:id', testsController.getTest)
+// router.delete('/:id', testsController.getTest)
+// // /tests/themes
+// router.post('/themes', testsController.getTest)
+// router.get('/themes/:theme_id', testsController.getTest)
+// router.put('/themes/:theme_id', testsController.getTest)
+// router.delete('/themes/:theme_id', testsController.getTest)
+// // /tests/themes/questions
+// router.post('/themes/:theme_id/questions', testsController.getTest)
+// router.get('/themes/:theme_id/questions/:question_id', testsController.getTest)
+// router.put('/themes/:theme_id/questions/:question_id', testsController.getTest)
+// router.delete('/themes/:theme_id/questions/:question_id', testsController.getTest)
+// // /tests/themes/questions/answers
+// router.post('/themes/:theme_id/questions/:question_id', testsController.getTest)
+// router.get('/themes/:theme_id/questions/:question_id', testsController.getTest)
+// router.put('/themes/:theme_id/questions/:question_id/answers/:answer_id', testsController.getTest)
+// router.delete('/themes/:theme_id/questions/:question_id/answers/:answer_id', testsController.getTest)
 export default router;
